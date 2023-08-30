3c2826491f970665c8424d912b037a8236f4ff65
Andresr35 <andresjruiz@cpp.edu>
2023-08-30 10:37:06 -0700

    added images to ships

--- src/components/ship.js
+++ src/components/ship.js
@@ -1,10 +1,36 @@
+import Carrier from "../images/carrier.svg";
+import Destroyer from "../images/destroyer.svg";
+import Patrol from "../images/patrol.svg";
+import Battleship from "../images/battleship.svg";
+
 const Ship = (length = 3, coords = [[]]) => {
   let timesHit = 0;
+  const shipImage = new Image();
   const sunk = false;
+
   const hit = () => {
     timesHit += 1;
   };
+
   const isSunk = () => length === timesHit;
+
+  switch (length) {
+    case 2:
+      shipImage.src = Patrol;
+      break;
+    case 3:
+      shipImage.src = Destroyer;
+      break;
+    case 4:
+      shipImage.src = Battleship;
+      break;
+    case 5:
+      shipImage.src = Carrier;
+      break;
+    default:
+      break;
+  }
+
   return { length, timesHit, sunk, hit, isSunk, coords };
 };
 
